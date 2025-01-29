f63552c4fd3915b178d7de2dc4f803c0710d00ef
Terraform Deployment <terraform@example.com>
2025-01-29 13:26:30 -0800

    fix syntax

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
 """
-Serializer for user.
+Serializer for user API View.
 """
 from django.contrib.auth import (
     get_user_model,
@@ -15,12 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {
-            'password': {
-                'write_only': True,
-                'min_length': 5,
-            }
-        }
+        extra_kwargs = {'password': {'write_only': True,'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return a new user."""
